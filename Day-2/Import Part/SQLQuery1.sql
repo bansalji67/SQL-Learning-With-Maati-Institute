5a0d88733905d36607575dc222c67bca46eec4b5
bansalji67 <sumit.bansal221@gmail.com>
2026-04-25 11:49:13 +0530

    updated

---
+++ Day-2/Import Part/SQLQuery1.sql
@@ -0,0 +1,24 @@
+
+Drop Table Example1
+Create Table  Example1(
+EmpID Int,
+EmpName Varchar(max),
+Salary bigint
+)
+
+Insert into Example1 Values(111,'Raj',23232)
+Insert into Example1 Values(222,'Rajesh',8766)
+Insert into Example1 Values(333,'Rahul',9866)
+Insert into Example1 Values(444,'Rakesh',2987)
+
+Insert into Example1 Values(555,'Radhey',6543)
+Insert into Example1 Values(666,'Romeo',9876)
+Insert into Example1 Values(777,'Raju',2449)
+
+Insert into Example1 Values(999,'Kunal',9876)
+Insert into Example1 Values(908,'Karan',4567)
+Insert into Example1 Values(1011,'Kaku',2398)
+
+Insert into Example1 Values(132,'Kiran',6789)
+Insert into Example1 Values(965,'Karishma',7634)
+Insert into Example1 Values(1123,'Komal',4567)
